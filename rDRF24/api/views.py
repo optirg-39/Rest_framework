433fa347a290260d9ffc89231ab618dadae3ec6f
rishabh gupta <opti39rg@gmail.com>
2021-04-07 18:52:14 +0530

    generate Token through Admin application and drf_create_token command

---
+++ rDRF24/api/views.py
@@ -0,0 +1,100 @@
+# ModelViewSet class
+from .models import Students
+from .serializers import StudentModelSerializer
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+class StudentModelViewSet(viewsets.ModelViewSet):
+        queryset = Students.objects.all()
+        serializer_class = StudentModelSerializer
+        authentication_classes = [TokenAuthentication]
+        permission_classes = [IsAuthenticated]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        pass
