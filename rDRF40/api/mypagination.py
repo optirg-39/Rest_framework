c94cbc93c9b0e358aa083e88526719dc03009035
rishabh gupta <opti39rg@gmail.com>
2021-04-11 13:19:34 +0530

    CursorPagination

--- rDRF40/api/mypagination.py
+++ rDRF40/api/mypagination.py
@@ -1,6 +1,8 @@
 from rest_framework.pagination import CursorPagination
 
-class MyLimitOffsetPagination(CursorPagination):
-    pass
+class MyCursorPagination(CursorPagination):
+    page_size = 5
+    ordering = 'name'
+    cursor_query_param = 'cu'
 
 
--- rDRF40/api/views.py
+++ rDRF40/api/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render
 from .serializers import StudentSerializer
 from .models import Student
 from rest_framework.generics import ListAPIView
-from .mypagination import MyLimitOffsetPagination
+from .mypagination import MyCursorPagination
 
 # Create your views here.
 class StudentListView(ListAPIView):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
-    pagination_class = MyLimitOffsetPagination
+    pagination_class = MyCursorPagination
 
 
