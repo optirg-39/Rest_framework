b5ebf72069a686ec278cc330eef3692cc4f9e2a7
rishabh gupta <opti39rg@gmail.com>
2021-04-11 19:18:44 +0530

    Serializer relation fields

---
+++ rDRF41/api/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from .serializer import SingerSerializer,SongSerializer
+from .models import Song, Singer
+from rest_framework import viewsets
+
+# Create your views here.
+class SingerViewSet(viewsets.ModelViewSet):
+    queryset = Singer.objects.all()
+    serializer_class = SingerSerializer
+
+class SongViewSet(viewsets.ModelViewSet):
+    queryset = Song.objects.all()
+    serializer_class = SongSerializer
+
+
